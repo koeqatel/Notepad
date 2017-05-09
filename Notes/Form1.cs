7d754396b1fc7aeafc1825f2df1d80f193c18300
Menno van Leeuwen <menno.worldwar132@gmail.com>
2017-05-09 19:54:46 +0200

    Dude, rewrote everything... the old way was VERY resource intense and inefficient :P

---
+++ Notes/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net;
+using System.Windows.Forms;
+
+namespace Notes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            editor.Visible = false;
+            editor.Text = new UserData(true).notesHtml;
+
+            viewer.DocumentText = getPrerequisites() + editor.Text;
+        }
+
+        public string getPrerequisites()
+        {
+            string jquery = new WebClient().DownloadString("https://code.jquery.com/jquery-1.12.4.min.js");
+
+            return "<script>" + jquery + "</script>";
+        }
+
+        private void editToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (editToolStripMenuItem.Text == "Edit")
+            {
+                editToolStripMenuItem.Text = "Done";
+
+                editor.Visible = true;
+                viewer.Visible = false;
+            }
+            else
+            {
+                editToolStripMenuItem.Text = "Edit";
+                viewer.DocumentText = getPrerequisites() + editor.Text;
+
+                editor.Visible = false;
+                viewer.Visible = true;
+            }
+
+            new UserData(editor.Text).SaveToFile();
+        }
+    }
+}
---
+++ Notes/UserData.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes
+{
+    public class UserData
+    {
+
+        public UserData(string html)
+        {
+            notesHtml = html;
+        }
+
+        public UserData(bool loadFromFile)
+        {
+            string s = File.ReadAllText(Environment.CurrentDirectory + "/userData.json");
+            notesHtml = JsonConvert.DeserializeObject<UserData>(s).notesHtml;
+        }
+
+        public UserData()
+        { }
+
+        public string notesHtml { get; set; }
+
+        public void SaveToFile()
+        {
+            //Save to JSON
+            string s = JsonConvert.SerializeObject(this);
+            File.WriteAllText(Environment.CurrentDirectory + "/userData.json", s);
+        }
+    }
+}
